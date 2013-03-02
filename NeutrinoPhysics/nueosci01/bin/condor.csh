f906581abe63f5592df88958592ed484fbdf5253
andres0sorio <osorio.af@gmail.com>
2013-03-02 17:26:57 -0600

    scripts for batch submission

---
+++ NeutrinoPhysics/nueosci01/bin/condor.csh
@@ -0,0 +1,21 @@
+#! /bin/tcsh -f
+
+setenv MYDIR /uscms_data/d2/aosorio/scratch0/CMSSW_5_3_4/run
+unsetenv CMS_PATH
+source /uscmst1/prod/sw/cms/cshrc prod
+pushd $MYDIR
+cmsenv
+popd
+
+setenv GSLPATH `scram tool tag gsl GSL_BASE`
+setenv BOOSTPATH `scram tool tag boost BOOST_BASE`
+
+setenv EMIN $1
+setenv EMAX $2
+
+setenv CMD  "./paper01.exe --model ModelA --prob Pee,Pem,Pet --erange "$EMIN","$EMAX
+
+echo INFO:  launching command: $CMD
+
+$CMD
+echo DONE
---
+++ NeutrinoPhysics/nueosci01/bin/submitCondor.py
@@ -0,0 +1,52 @@
+import sys, os, stat, shutil
+#--------------------------------------------------------
+#--------------------------------------------------------
+
+arguments = []
+
+xmax = 1.0e14
+x0   = 1.0e11
+dx   = 10.0
+
+while x0 < xmax:
+    x1 = x0*dx
+    erange = str(x0)+','+str(x1)
+    arguments.append( erange.split(',') )
+    x0 = x1
+
+print arguments
+
+input_sandbox = ['matrix_config.xml',
+                 'matrix_config.dtd',
+                 'model_config.xml',
+                 'model_config.dtd',
+                 'paper01.exe']
+
+script = './condor.csh'
+output = 'output.root'
+#--------------------------------------------------------
+
+app = Executable( exe=File( script ) )
+
+sp = ArgSplitter()
+sp.args = arguments
+
+mg = RootMerger()
+mg.files=[output]
+mg.ignorefailed = True
+
+#Job declaration and initialisation:
+myjob = Job( application = app, backend = 'Condor' )
+myjob.name = 'nueosc.cmssw'
+myjob.splitter = sp
+myjob.merger = mg
+
+myjob.outputsandbox = [output]
+
+for input in input_sandbox:
+    myjob.inputsandbox.append( File ( input ) )
+
+myjob.submit()
+
+print "job submission done."
+
