97c86329c9e01ccd6ed97b3416c9abcf1b25e22a
andres0sorio <osorio.af@gmail.com>
2014-10-08 23:42:22 -0500

    Added PAPER_DATA environment to setenv.sh

--- NeutrinoPhysics/nueosci02/bin/setenv.sh
+++ NeutrinoPhysics/nueosci02/bin/setenv.sh
@@ -1,4 +1,9 @@
 
 GRBWORKPATH=/scratch/Exploration/GIT/GRBsWork/NeutrinoPhysics
 
+PAPER_DATA=/scratch/Exploration/GIT/GRBsWork/NeutrinoPhysics/nueosci02/data
+
 export LD_LIBRARY_PATH=$GRBWORKPATH/nueoscilib/lib:$LD_LIBRARY_PATH
+
+export PAPER_DATA
+
