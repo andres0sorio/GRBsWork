9506369dc3b14aea77994de7ff1fadcc1b7786c2
andres0sorio <osorio.af@gmail.com>
2014-11-05 00:52:17 -0500

    * update of submitHepServ-RvsAlfa.py script

--- NeutrinoPhysics/nueosci02/bin/submitHepServ-RvsAlfa.py
+++ NeutrinoPhysics/nueosci02/bin/submitHepServ-RvsAlfa.py
@@ -22,6 +22,8 @@ dcp_vars = ['0','180']
 
 dmass_vars = ['0.0014','0.006','0.0032']
 
+config = xmlfi.split('_')[2]
+
 for dcp in dcp_vars:
     for dmass in dmass_vars:
         dmasses = dmass + ',' + '0.00008'
@@ -59,7 +61,7 @@ mg.ignorefailed = True
 
 #Job declaration and initialisation:
 myjob = Job( application = app, backend = 'Local' )
-myjob.name = 'nueosc.StdPicture'
+myjob.name = 'RvsAlfa.StdPicture.' + config
 myjob.splitter = sp
 myjob.merger = mg
 
